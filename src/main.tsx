ba42f6ddace516660634347391b4a7ba31059e17
千葉 智貴 <132980385+kentem-to-chiba@users.noreply.github.com>
2024-07-19 13:51:15 +0900

    feat: Add react-router-dom dependency and create root route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+	},
+	{
+		path: "/app",
+		element: <App />,
+	},
+]);
 
 const root = document.getElementById("root");
 if (root)
 	ReactDOM.createRoot(root).render(
 		<React.StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</React.StrictMode>,
 	);
---
+++ src/routes/root.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Root() {
+	return (
+		<ul>
+			<li>
+				<Link to="/app">App</Link>
+			</li>
+		</ul>
+	);
+}
+export default Root;
