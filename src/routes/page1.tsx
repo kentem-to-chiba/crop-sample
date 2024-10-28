59bee72b9f86cb5c4dde314e8d8410c5d0ba55dd
千葉 智貴 <132980385+kentem-to-chiba@users.noreply.github.com>
2024-10-29 08:58:42 +0900

    feat: aspect固定の切り替え機能

--- src/routes/page1.tsx
+++ src/routes/page1.tsx
@@ -4,15 +4,23 @@ import sampleImg from "../assets/cat.png";
 
 function Page1() {
 	const [crop, setCrop] = useState<Crop>();
+	const [aspect, setAspect] = useState<number | undefined>(5 / 6);
 
 	useEffect(() => {
 		console.log(crop);
 	}, [crop]);
 
 	return (
-		<ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
-			<img src={sampleImg} alt="sample" />
-		</ReactCrop>
+		<>
+			<input
+				type="checkbox"
+				checked={aspect === 5 / 6}
+				onChange={() => setAspect((prev) => (prev ? undefined : 5 / 6))}
+			/>
+			<ReactCrop crop={crop} onChange={(c) => setCrop(c)} aspect={aspect}>
+				<img src={sampleImg} alt="sample" />
+			</ReactCrop>
+		</>
 	);
 }
 
